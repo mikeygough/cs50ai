4dc2aa2c62a28832b0888941fff148bb51399f80
mikey <mikey@hey.com>
2023-08-11 11:28:17 -0500

    add degrees assignment (DFS, BFS)

---
+++ week0-search/degrees/output.py
@@ -0,0 +1,63 @@
+names = {
+    "kevin bacon": {"102"},
+    "tom cruise": {"129"},
+    "cary elwes": {"144"},
+    "tom hanks": {"158"},
+    "mandy patinkin": {"1597"},
+    "dustin hoffman": {"163"},
+    "chris sarandon": {"1697"},
+    "demi moore": {"193"},
+    "jack nicholson": {"197"},
+    "bill paxton": {"200"},
+    "sally field": {"398"},
+    "valeria golino": {"420"},
+    "gerald r. molen": {"596520"},
+    "gary sinise": {"641"},
+    "robin wright": {"705"},
+    "emma watson": {"914612"},
+}
+people = {
+    "102": {"name": "Kevin Bacon", "birth": "1958", "movies": {"112384", "104257"}},
+    "129": {"name": "Tom Cruise", "birth": "1962", "movies": {"95953", "104257"}},
+    "144": {"name": "Cary Elwes", "birth": "1962", "movies": {"93779"}},
+    "158": {"name": "Tom Hanks", "birth": "1956", "movies": {"112384", "109830"}},
+    "1597": {"name": "Mandy Patinkin", "birth": "1952", "movies": {"93779"}},
+    "163": {"name": "Dustin Hoffman", "birth": "1937", "movies": {"95953"}},
+    "1697": {"name": "Chris Sarandon", "birth": "1942", "movies": {"93779"}},
+    "193": {"name": "Demi Moore", "birth": "1962", "movies": {"104257"}},
+    "197": {"name": "Jack Nicholson", "birth": "1937", "movies": {"104257"}},
+    "200": {"name": "Bill Paxton", "birth": "1955", "movies": {"112384"}},
+    "398": {"name": "Sally Field", "birth": "1946", "movies": {"109830"}},
+    "420": {"name": "Valeria Golino", "birth": "1965", "movies": {"95953"}},
+    "596520": {"name": "Gerald R. Molen", "birth": "1935", "movies": {"95953"}},
+    "641": {"name": "Gary Sinise", "birth": "1955", "movies": {"112384", "109830"}},
+    "705": {"name": "Robin Wright", "birth": "1966", "movies": {"93779", "109830"}},
+    "914612": {"name": "Emma Watson", "birth": "1990", "movies": set()},
+}
+moives = {
+    "112384": {
+        "title": "Apollo 13",
+        "year": "1995",
+        "stars": {"200", "641", "102", "158"},
+    },
+    "104257": {
+        "title": "A Few Good Men",
+        "year": "1992",
+        "stars": {"193", "197", "102", "129"},
+    },
+    "109830": {
+        "title": "Forrest Gump",
+        "year": "1994",
+        "stars": {"641", "398", "705", "158"},
+    },
+    "93779": {
+        "title": "The Princess Bride",
+        "year": "1987",
+        "stars": {"1597", "144", "705", "1697"},
+    },
+    "95953": {
+        "title": "Rain Man",
+        "year": "1988",
+        "stars": {"596520", "420", "163", "129"},
+    },
+}
